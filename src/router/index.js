74c5c4e545d25bf66a61059a71f9a42fb08d48d2
TommyK28 <tomaskricensky@persoo.cz>
2024-02-18 18:32:03 +0100

    add paginations

--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,29 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import EventListView from '../views/EventListView.vue'
-import EventDetaislView from '../views/EventDetailsView.vue'
-import AboutView from '../views/AboutView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import EventListView from "../views/EventListView.vue";
+import EventDetaislView from "../views/EventDetailsView.vue";
+import AboutView from "../views/AboutView.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'event-list',
-      component: EventListView
-    },
-    {
-      path: '/event/:id',
-      name: 'event-details',
-      props: true,
-      component: EventDetaislView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: AboutView
-    }
-  ]
-})
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: "/",
+            name: "event-list",
+            component: EventListView,
+            props: (route) => ({ page: parseInt(route.query.page) || 1 }),
+        },
+        {
+            path: "/event/:id",
+            name: "event-details",
+            props: true,
+            component: EventDetaislView,
+        },
+        {
+            path: "/about",
+            name: "about",
+            component: AboutView,
+        },
+    ],
+});
 
-export default router
+export default router;
